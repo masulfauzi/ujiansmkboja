4567d80ae2c05e2ec75f63fce8459c0e5c3a54eb
Mas'ul <masul.fauzi@gmail.com>
2025-01-27 22:19:37 +0700

    fix: upload config and example

---
+++ config/config.candy.php
@@ -0,0 +1,9 @@
+<?php
+$copyright = 'Candy CBT';
+
+define("VERSI", "2.9.2");
+define("REVISI", "1");
+define("APLIKASI", "X-Candy CBT");
+define("NAMA_DATABASE", "cbtcandy28");
+define("VERSI_DB", "2.9.2");
+define("BASEPATH", "cbtcandy");
